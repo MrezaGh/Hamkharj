ddc71285b152d7e3bc69b4e83c3b782ea9f7535f
Alireza Fatemi <alirezafa@rocketmail.com>
2019-10-31 10:33:05 +0330

    :recycle: Refactoring code.

--- hamkharj/wsgi.py
+++ hamkharj/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for djangox_project project.
+WSGI config for hamkharj project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangox_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hamkharj.settings')
 
 application = get_wsgi_application()
