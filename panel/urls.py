a3167b9b5b4c006b0ee359b680b712b6aa864a20
Alireza Fatemi <alirezafa@rocketmail.com>
2019-10-31 23:11:46 +0330

    :ambulance: Critical hotfix.

--- panel/urls.py
+++ panel/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', HomePageView, name='home'),
+    path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
 ]
--- panel/views.py
+++ panel/views.py
@@ -1,9 +1,8 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-@login_required
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = 'pages/home.html'
 
 
