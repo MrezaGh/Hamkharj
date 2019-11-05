34be50fed4ab57c4a6c1f3c9f73eb80c1368bf24
ce441-981-95105432 <pakzadarezou@gmail.com>
2019-11-05 16:08:35 +0330

    class Expense in modules created

--- panel/models.py
+++ panel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Expense(models.Model):
+
+    user = models.ForeignKey(User, models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    amount = models.DecimalField(name="Amount", max_digits=9)
+    description = models.TextField(blank=True)
+    title = models.CharField(max_length=200)
+
+
+
+
+
