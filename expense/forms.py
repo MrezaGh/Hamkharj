0e64858b352b7ab3fef913d1e064f07a11d7a735
Soroosh Baselizadeh <soroosh.baselizadeh@gmail.com>
2019-11-09 17:39:35 +0330

    form with diff creator

--- expense/forms.py
+++ expense/forms.py
@@ -5,55 +5,55 @@ from users.models import CustomUser
 
 
 class ExpenseForm(forms.ModelForm):
+
     def __init__(self, *args, **kwargs):
         self.creator = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
         users = CustomUser.objects.all()
-        self.users = dict()
-        self.fields["title"] = forms.CharField(max_length=200)
-        self.fields["description"] = forms.CharField(max_length=400)
-        self.fields["amount"] = forms.DecimalField(max_digits=9, decimal_places=0)
+        self.users = list()
+        self.fields['title'] = forms.CharField(max_length=200)
+        self.fields['description'] = forms.CharField(max_length=400)
+        self.fields['amount'] = forms.DecimalField(max_digits=9, decimal_places=0)
 
         for i, user in enumerate(users):
-            field_name = "user_{}_share".format(i)
+            field_name = 'user_%s_share' % (user.email,)
             self.fields[field_name] = forms.DecimalField(max_digits=9, decimal_places=1)
-            self.users[field_name] = user
+            self.users.append(user)
             self.initial[field_name] = 0
 
     def clean(self):
-        temps = set()
         i = 0
-        field_name = "user_{}_share".format(i)
-        users = list()
+        field_name = 'user_%s_share' % (self.users[i].email, )
         percentages = list()
         while self.cleaned_data.get(field_name):
             temp = self.cleaned_data[field_name]
-            users.append(self.users[field_name])
             percentages.append(temp)
-            temps.add(temp)
             i += 1
-            field_name = "user_{}_share".format(i)
-        self.cleaned_data["users-expenses"] = {
-            "users": users,
-            "percentages": percentages,
-        }
+            if i < len(self.users):
+                field_name = 'user_%s_share' % (self.users[i].email, )
+            else:
+                break
+        self.cleaned_data['users-expenses'] = {"users": self.users, "percentages": percentages}
 
-    def save(self, **kwargs):
+    def save(self):
         expense = self.instance
-        expense.title = self.cleaned_data["title"]
-        expense.description = self.cleaned_data["description"]
-        expense.amount = self.cleaned_data["amount"]
-        expense.creator = self.creator
-
+        expense.title = self.cleaned_data['title']
+        expense.description = self.cleaned_data['description']
+        expense.amount = self.cleaned_data['amount']
+        expense.creator = self.cleaned_data['creator']
         expense.save()
-        for user, percentage in zip(
-            self.cleaned_data["users-expenses"]["users"],
-            self.cleaned_data["users-expenses"]["percentages"],
-        ):
+        for user, percentage in zip(self.cleaned_data['users-expenses']['users'], self.cleaned_data['users-expenses']['percentages']):
             Record.objects.create(
-                expense=expense, user=user, percent_of_share=percentage,
+                expense=expense,
+                user=user,
+                percent_of_share=percentage,
             )
 
     class Meta:
         model = Expense
-        fields = ["title", "amount", "description"]
+        fields = [
+            'title',
+            'creator',
+            'amount',
+            'description'
+        ]
