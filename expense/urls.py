cf3dce1c40d52ae6aa623afc4edcd00bb6b569c8
abdijavad110 <abdijavad110@gmail.com>
2019-12-20 19:53:18 +0330

    new:dev: fix minor bugs

--- expense/urls.py
+++ expense/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import expense_create_view
 
+
 urlpatterns = [
     path("create-expense/", expense_create_view, name="create_expense"),
     path("create-expense/<int:group_id>", expense_create_view, name="create_expense"),
--- hamkharj/urls.py
+++ hamkharj/urls.py
@@ -1,8 +1,17 @@
-from django.conf import settings
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+# fixme should be deleted
+def landing(request):
+    return redirect("panel")
+
 
 urlpatterns = [
+    # fixme should be deleted
+    path("", landing, name="landing"),
     path("admin/", admin.site.urls),
     path("users/", include("django.contrib.auth.urls")),
     path("accounts/", include("allauth.urls")),
