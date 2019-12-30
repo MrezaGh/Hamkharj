9cdecd51dbd7317b104222d059b5d1d023500e40
alirezasgr99 <alireza.samiei7873@gmail.com>
2019-12-30 07:24:44 +0330

    refactor expense form add expense_attachment to expense form

--- expense/forms.py
+++ expense/forms.py
@@ -1,78 +1,41 @@
+import re
+
 from django import forms
 
 from expense.models import Expense, Record
 from group.models import Group
-from django.conf import settings
-from PIL import Image, ImageOps, ImageFile
+
+
+user_share_pattern = re.compile(r'^user_(?P<user_id>\d+)_share$')
+
+
 class ExpenseForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop("user", None)
-        self.g_id = kwargs.pop("g_id", None)
+        gid = kwargs.pop("gid", None)
+        group = Group.objects.get(pk=gid)
+        group_users = group.users.values_list('id', 'email')
         super().__init__(*args, **kwargs)
-        group = Group.objects.get(pk=self.g_id)
-        users = list(group.users.all())
-
-        group_users = list()
-        for user in users:
-            group_users.append((user, user.email))
-        self.users_dict = dict()
-        for user in group_users:
-            self.users_dict[user[1]] = user[0]
-
-        self.users = list()
-        self.fields["creator"] = forms.ChoiceField(label="payer", choices=group_users)
-        self.fields["title"] = forms.CharField(max_length=200)
-        self.fields["description"] = forms.CharField(max_length=400, required=False)
-        self.fields["amount"] = forms.DecimalField(max_digits=9, decimal_places=0)
-        # self.expense_image = forms.ImageField()
-
-
-        for i, user in enumerate(users):
-            field_name = "user_%s_share" % (user.email,)
-            self.fields[field_name] = forms.DecimalField(max_digits=9, decimal_places=1)
-            self.users.append(user)
+        self.fields["creator"].choices = group_users
+        for user_id, user_email in group_users:
+            field_name = f"user_{user_id}_share"
+            self.fields[field_name] = forms.DecimalField(
+                max_digits=9,
+                decimal_places=1,
+                label=f'share of user {user_email}'
+            )
             self.initial[field_name] = 0
 
-    def clean(self):
-        i = 0
-        field_name = "user_%s_share" % (self.users[i].email,)
-        percentages = list()
-        while self.cleaned_data.get(field_name):
-            temp = self.cleaned_data[field_name]
-            percentages.append(temp)
-            i += 1
-            if i < len(self.users):
-                field_name = "user_%s_share" % (self.users[i].email,)
-            else:
-                break
-        self.cleaned_data["users-expenses"] = {
-            "users": self.users,
-            "percentages": percentages,
-        }
-
-
     def save(self, **kwargs):
-        expense = self.instance
-        expense.title = self.cleaned_data["title"]
-        expense.description = self.cleaned_data["description"]
-        expense.amount = self.cleaned_data["amount"]
-        expense.creator = self.users_dict[self.cleaned_data["creator"]]
-        # print(type(self.cleaned_data["expense_image"]), self.cleaned_data["expense_image"])
-        # print(self.expense_image)
-        # mirrored_image = ImageOps.mirror(self.expense_image)
-        # image_file = ImageFile(mirrored_image)
-        # expense.expense_image = image_file
-        # expense.expense_image = self.expense_image
-        expense.save()
-        for user, percentage in zip(
-            self.cleaned_data["users-expenses"]["users"],
-            self.cleaned_data["users-expenses"]["percentages"],
-        ):
-            if user.email != self.cleaned_data["creator"]:
+        expense = super(ExpenseForm, self).save(**kwargs)
+        for key in filter(user_share_pattern.match,  self.cleaned_data):
+            user_id = int(user_share_pattern.match(key)['user_id'])
+            percentage = self.cleaned_data[key]
+            if user_id != self.cleaned_data["creator"].id:
                 Record.objects.create(
-                    expense=expense, user=user, percent_of_share=percentage,
+                    expense=expense, user_id=user_id, percent_of_share=percentage,
                 )
+        return expense
 
     class Meta:
         model = Expense
-        fields = ["title", "amount", "description"]
+        fields = ["title", "amount", "category", "description", "expense_attachment", "creator"]
