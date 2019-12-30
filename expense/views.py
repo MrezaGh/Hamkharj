5222e54ca34b184d10a380d249fbf42f21da2670
alirezasgr99 <alireza.samiei7873@gmail.com>
2019-12-30 07:26:52 +0330

    use django class based create view to create expenses add expense attachement to summary view

--- expense/views.py
+++ expense/views.py
@@ -1,18 +1,23 @@
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 from .forms import ExpenseForm
 from .models import Expense, Record
 
 
-@login_required
-def expense_create_view(request, **kwargs):
-    form = ExpenseForm(request.POST or None, user=request.user, g_id=kwargs.pop('group_id'))
-    if form.is_valid():
-        form.save()
-        return redirect("panel")
-    context = {"form": form}
-    return render(request, "pages/create_expense.html", context)
+class ExpenseCreateView(CreateView):
+    template_name = 'pages/create_expense.html'
+    form_class = ExpenseForm
+    success_url = reverse_lazy('panel')
+
+    def get_form_kwargs(self):
+        kwargs = super(ExpenseCreateView, self).get_form_kwargs()
+        kwargs.update(gid=self.kwargs['group_id'])
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        return {**super(ExpenseCreateView, self).get_context_data(**kwargs), **{'group_id': self.kwargs['group_id']}}
 
 
 def summary(request):
@@ -22,6 +27,12 @@ def summary(request):
         # paid_expenses = Expense()
         records_of_paid_expenses = Record.objects.filter(expense=paid_expense)
         if len(records_of_paid_expenses):
-            in_debt_users[paid_expense.title] = [(paid_expense.amount,record.user, record.percent_of_share * paid_expense.amount/100) for record in records_of_paid_expenses]
+            in_debt_users[paid_expense.title] = [(
+                paid_expense.amount,
+                paid_expense.expense_attachment.url if paid_expense.expense_attachment else '',
+                record.user,
+                record.percent_of_share * paid_expense.amount / 100
+            ) for record in records_of_paid_expenses]
 
-    return render(request, 'pages/summary_of_expenses.html', context={'expenses': in_debt_users, 'creator':request.user})
+    return render(request, 'pages/summary_of_expenses.html',
+                  context={'expenses': in_debt_users, 'creator': request.user})
