1f342855a36cceb846cbbe2aa7ffabb2a8f7110a
alirezasgr99 <alireza.samiei7873@gmail.com>
2020-02-08 01:40:06 +0330

    add new view for user recent activities provide urlconf and template for user activities view

--- hamkharj/urls.py
+++ hamkharj/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("django.contrib.auth.urls")),
     path("accounts/", include("allauth.urls")),
-    path("accounts/settings", include("users.urls")),
+    path("accounts/settings/", include("users.urls")),
     path(r"expenses/", include("expense.urls")),
     path("group/", include("group.urls")),
     path("friend/", include("friend.urls")),
