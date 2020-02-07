ca5ea7624169198cc49d54bb2930bffef0cfb8e4
alirezasgr99 <alireza.samiei7873@gmail.com>
2020-02-08 01:36:06 +0330

    add Historical Records manager to Expense, ExpenseCategory, Record, Group and Friendship register Invitation on simple history

--- friend/models.py
+++ friend/models.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.db import models
 
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+
 class Friendship(models.Model):
+    history = HistoricalRecords()
+
     user_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="current_user")
     friend_id = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="friend")
     friendship_start_date = models.DateTimeField(auto_now_add=True)
--- group/models.py
+++ group/models.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.db import models
+from simple_history.models import HistoricalRecords
 
 
 class Group(models.Model):
+    history = HistoricalRecords()
+
     title = models.CharField(max_length=200, default="My Group")
     creator = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="owner"
@@ -12,3 +15,6 @@ class Group(models.Model):
 
     date_added = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
