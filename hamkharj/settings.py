6943ea81f503c6ea7859801d0b234b67db345dca
Mreza Gh <mrezagh.legend@gmail.com>
2020-02-08 17:45:22 +0330

    :email:new:dev: Support Email
    Email verification and invite by mail completely functional

--- hamkharj/settings.py
+++ hamkharj/settings.py
@@ -137,8 +137,12 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "hamkharj.taromi@gmail.com"
+EMAIL_HOST_PASSWORD = "123456seven!"
 # Django-Allauth Config
 
 LOGIN_REDIRECT_URL = "panel"
